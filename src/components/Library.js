4850ee3d9d7992dc6a80bc7165ff542068e02e35
maxsumy <grenki2@gmail.com>
2021-01-04 19:16:36 +0200

    6_06

---
+++ src/components/Library.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import LibrarySong from "./LibrarySong";
+
+const Library =({songs, setCurrentSong})=>{
+    return(
+        <div className="library">
+            <h2>library</h2>
+            <div className="library-songs">
+                {songs.map((song)=>(
+                    <LibrarySong
+                        songs={songs}
+                        setCurrentSong={setCurrentSong}
+                        song={song}
+                        id={song.id}
+                        key={song.id}
+                    />
+                ))
+
+                }
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Library;
---
+++ src/util.js
@@ -0,0 +1,71 @@
+import {v4 as uuidv4} from "uuid";
+
+
+function chilHopp() {
+    return [
+        {
+            name: "ny90",
+            artist: "Ezzy",
+            cover: "https://chillhop.com/wp-content/uploads/2020/12/7e98d3028a22ee7f16f6a9bfcdc2089f089777a5-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#BA4A46", "FDF0DD"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=7814",
+        },
+
+        {
+            name: "Seascape",
+            artist: "Makzo",
+            cover: "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#BA4A46", "FDF0DD"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=11773",
+        },
+
+        {
+            name: "Peaches",
+            artist: "Philanthrope, The Field Tapes",
+            cover: "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#BA4A46", "FDF0DD"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=11246",
+        },
+
+        {
+            name: "Les Mouvements d'Hiver",
+            artist: "L'Indécis, sadtoi",
+            cover: "https://chillhop.com/wp-content/uploads/2020/10/d2898bc0ef36b5d67d6793062d588d208a90421c-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#BA4A46", "FDF0DD"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10351",
+        },
+
+        {
+            name: "5 am",
+            artist: "Ruck P, Shuko",
+            cover: "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#BA4A46", "FDF0DD"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10309",
+        },
+
+        {
+            name: "Les Mouvements d'Hiver",
+            artist: "L'Indécis, sadtoi",
+            cover: "https://chillhop.com/wp-content/uploads/2020/10/d2898bc0ef36b5d67d6793062d588d208a90421c-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#BA4A46", "FDF0DD"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10351",
+        },
+
+    ];
+
+}
+
+export default chilHopp;
+
